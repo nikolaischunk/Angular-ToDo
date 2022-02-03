58dab10d3672939f3361ed12c9b7a79e27a7903e
Nikolai Schunk <nikolai.schunk@contracted.pmi.com>
2022-02-03 10:27:41 +0100

    Added first Toggle Implementation with TS

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-list',
   templateUrl: './page-list.component.html',
-  styleUrls: ['./page-list.component.scss']
+  styleUrls: ['./page-list.component.scss'],
 })
 export class PageListComponent implements OnInit {
+  public toDoShow: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.toDoShow = true;
   }
 
+  ngOnInit(): void {}
 }
